7c261edec153122268bb2936d72e7c222fe5257c
Chilly-17 <matko.ciljak.pk@gmail.com>
2023-07-12 23:06:06 +0200

    Learning pygame
    https://www.youtube.com/watch?v=jO6qQDNa2UY - 17:07
    
    Learned:
    - Display update
    - Fixed FPS
    - Quit event

---
+++ spaceinvader/invader1.py
@@ -0,0 +1,28 @@
+import pygame
+
+WHITE = (255, 255, 255)
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space 1v1 by Chilly")
+
+
+def draw_window():
+    WIN.fill(WHITE)
+    pygame.display.update()
+
+
+def main():
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        draw_window()
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
